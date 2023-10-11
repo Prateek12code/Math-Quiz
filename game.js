d7a287bc45a3365290cfdff0d8c647a512bc9506
Prateek12code <124058592+Prateek12code@users.noreply.github.com>
2023-10-11 18:01:15 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,14 @@
+function send(){
+    number1 = document.getElementById("number1").value;
+    number2 = document.getElementById("number2").value;
+    actual_answer = parseInt(number1) * parseInt(number2);
+
+    question_number = "<h4>" + number1 + " X "+ number2 + "</h4>";
+    input_box = "<br>Answer: <input type='text' id='input_check_box'>";
+    check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+    row = question_number + input_box + check_button ;
+
+    document.getElementById("output").innerHTML = row;
+    document.getElementById("number1").value = "";
+    document.getElementById("number2").value = "";
+}
