abc349bb77531233f680499b7d55f6d42a6a94c8
Prateek12code <124058592+Prateek12code@users.noreply.github.com>
2023-10-24 13:41:25 +0530

    Add files via upload

--- game.js
+++ game.js
@@ -1,3 +1,13 @@
+player1_name = document.getElementById("player1_name").innerHTML = localStorage.getItem("player1_name");
+player2_name = document.getElementById("player2_name").innerHTML = localStorage.getItem("player2_name");
+
+player1_score = 0 ;
+player2_score = 0 ;
+
+question_turn = "player1";
+answer_turn = "player2";
+
+
 function send(){
     number1 = document.getElementById("number1").value;
     number2 = document.getElementById("number2").value;
@@ -11,4 +21,44 @@ function send(){
     document.getElementById("output").innerHTML = row;
     document.getElementById("number1").value = "";
     document.getElementById("number2").value = "";
+}
+
+
+function check(){
+    get_answer = document.getElementById("ïnput_check_box").value;
+    if(get_answer == actual_answer)
+    {
+        if(answer_turn == "player1")
+        {
+            update_player1_score = player1_score +1;
+            document.getElementById("player1_score").innerHTML = update_player1_score;
+        }
+        else
+        {
+            update_player2_score = player2_score +1;
+            document.getElementById("player2_score").innerHTML = update_player2_score;
+        }
+    }
+}
+
+if(question_turn == "player1")
+{
+    question_turn = "player2";
+    document.getElementById("player_question").innerHTML = "Question Turn - " + player2_name ;
+}
+else
+{
+    question_turn = "player1";
+    document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name ;
+}
+
+if(answer_turn == "player1")
+{
+    question_turn = "player2";
+    document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name ;
+}
+else
+{
+    question_turn = "player1";
+    document.getElementById("player_answer").innerHTML = "Answer Turn - " + player1_name ;
 }
