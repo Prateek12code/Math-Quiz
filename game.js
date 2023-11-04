f1953e9a3da8978194573c42af8987fc0512682b
Prateek12code <124058592+Prateek12code@users.noreply.github.com>
2023-11-04 13:45:07 +0530

    Add files via upload

--- game.js
+++ game.js
@@ -4,9 +4,14 @@ player2_name = document.getElementById("player2_name").innerHTML = localStorage.
 player1_score = 0 ;
 player2_score = 0 ;
 
-question_turn = "player1";
-answer_turn = "player2";
+document.getElementById("player1_name").innerHTML = player1_name + " : ";
+document.getElementById("player2_name").innerHTML = player2_name + " : ";
 
+document.getElementById("player1_score").innerHTML = player1_score;
+document.getElementById("player2_score").innerHTML = player2_score;
+
+document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name;
+document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name;
 
 function send(){
     number1 = document.getElementById("number1").value;
@@ -23,42 +28,50 @@ function send(){
     document.getElementById("number2").value = "";
 }
 
+question_turn = "player1";
+answer_turn = "player2";
 
 function check(){
-    get_answer = document.getElementById("ïnput_check_box").value;
+
+    get_answer = document.getElementById("input_check_box").value;
     if(get_answer == actual_answer)
     {
         if(answer_turn == "player1")
         {
-            update_player1_score = player1_score +1;
-            document.getElementById("player1_score").innerHTML = update_player1_score;
+            player1_score = player1_score +1;
+            document.getElementById("player1_score").innerHTML = player1_score;
         }
         else
         {
-            update_player2_score = player2_score +1;
-            document.getElementById("player2_score").innerHTML = update_player2_score;
+            player2_score = player2_score +1;
+            document.getElementById("player2_score").innerHTML = player2_score;
         }
     }
-}
 
-if(question_turn == "player1")
-{
-    question_turn = "player2";
+
+
+    if(question_turn == "player1")
+   {
+    question_turn = "player2"
     document.getElementById("player_question").innerHTML = "Question Turn - " + player2_name ;
-}
-else
-{
-    question_turn = "player1";
+   }
+   else
+   {
+    question_turn = "player1"
     document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name ;
+   }
+
+   if(answer_turn == "player1")
+   {
+    answer_turn = "player2"
+    document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name ;
+   }
+   else
+   {
+    answer_turn = "player1"
+    document.getElementById("player_answer").innerHTML = "Answer Turn - " + player1_name ;
+   }
+
+   document.getElementById("output").innerHTML = "";
 }
 
-if(answer_turn == "player1")
-{
-    question_turn = "player2";
-    document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name ;
-}
-else
-{
-    question_turn = "player1";
-    document.getElementById("player_answer").innerHTML = "Answer Turn - " + player1_name ;
-}
